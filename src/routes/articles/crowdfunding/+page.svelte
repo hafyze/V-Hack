6a7af9583424aae5bf961eb614eb24da2342a76b
Reuben Lee Suppiah (Personal) <meisreuben@gmail.com>
2024-03-22 00:56:41 +0800

    Financial Articles

---
+++ src/routes/articles/crowdfunding/+page.svelte
@@ -0,0 +1,53 @@
+
+<div class="w-full h-full flex items-center flex-col mb-10">
+    <div class="w-full">
+        <button type="button" class="btn-icon variant-filled ml-12 mt-5" on:click={() => {history.back()}}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
+            </svg>
+        </button>
+    </div>
+    <div class="p-8 pt-0">
+        <span class="text-6xl font-bold">
+            Equity Crowdfunding Malaysia
+        </span>
+        <br>
+        <div class="mt-5">
+            <span class="italic font-thin">
+                written by 
+            </span>
+            <span class="italic">
+                John Doe 
+            </span>
+            <br>
+            <span class="italic font-thin">
+                20/3/2024
+            </span>
+        </div>
+    </div>
+    <div class="mb-5">
+        <img class="h-auto max-w-full rounded-lg basis-1/5" src="/src/lib/equityCrowdfunding.png" alt="">
+    </div>
+    <div class="flex flex-row px-52">
+        <div class="flex flex-col justify-center">
+            <p class="pt-4 text-justify">
+                Equity crowdfunding has emerged as a significant avenue for raising capital for startups and small and medium enterprises (SMEs) in Malaysia since its legalization by the Securities Commission (SC) in 2015. Prior to this, traditional avenues of funding such as bank loans or venture capital were often inaccessible to many promising ventures. With the introduction of equity crowdfunding, Malaysian entrepreneurs gained a new channel to attract investments from a wider pool of investors, fostering innovation and entrepreneurial growth in the country.
+            </p>
+            <p class="pt-4 text-justify">
+                One of the key players in Malaysia's equity crowdfunding landscape is pitchIN, a Registered Market Operator (RMO) approved by the Securities Commission. As an approved operator, pitchIN plays a crucial role in facilitating the crowdfunding process, connecting businesses seeking capital with potential investors. Through its platform, pitchIN provides a secure and regulated environment for both investors and entrepreneurs to engage in equity crowdfunding activities.
+            </p>
+            <p class="pt-4 text-justify">
+                The regulatory framework governing equity crowdfunding in Malaysia is outlined in the Guidelines on Recognized Markets (‘ECF Guidelines’) issued by the Securities Commission. These guidelines are designed to ensure investor protection, promote transparency, and maintain the integrity of the crowdfunding ecosystem. By adhering to these regulations, equity crowdfunding platforms like pitchIN are able to instill confidence among investors and businesses alike, fostering a conducive environment for capital formation.
+            </p>
+            <p class="pt-4 text-justify">
+                One of the distinctive features of equity crowdfunding is its ability to democratize investment opportunities, allowing ordinary individuals to participate in early-stage investments that were previously reserved for institutional investors or high-net-worth individuals. Through platforms like pitchIN, investors can browse through a variety of investment opportunities across different sectors and stages of development, enabling them to diversify their investment portfolios and potentially earn attractive returns.
+            </p>
+            <p class="pt-4 text-justify">
+                For entrepreneurs, equity crowdfunding offers more than just capital infusion. It provides an avenue to validate their business ideas, gain market traction, and cultivate a community of loyal supporters who are not just passive investors but also brand ambassadors. By engaging with investors through crowdfunding campaigns, entrepreneurs can leverage their expertise, networks, and feedback to refine their products or services, ultimately increasing their chances of success in the market.
+            </p>           
+            <p class="pt-4 text-justify">
+                In conclusion, equity crowdfunding has emerged as a vital component of Malaysia's entrepreneurial ecosystem, providing a regulated platform for businesses to raise capital and for investors to participate in early-stage investments. With players like pitchIN leading the way, the equity crowdfunding landscape in Malaysia continues to evolve, offering promising opportunities for innovation, growth, and economic development. As the ecosystem matures and gains further traction, it is expected to play an increasingly significant role in shaping the future of entrepreneurship and investment in Malaysia.
+            </p>
+        </div>
+    </div>
+</div>
---
+++ src/routes/book/+page.svelte
@@ -0,0 +1,67 @@
+<script>
+
+  import { goto } from "$app/navigation";
+
+</script>
+<div class=" w-full p-8">
+    <span class="h1">Financial Articles</span>
+</div>
+
+<div class="w-full h-full flex items-center flex-col">
+    <!-- ITEM 1 -->
+    <span class="horizontalLine my-12"/>
+    <div class="flex flex-row px-32">
+        <div class="flex flex-col basis-4/5 justify-center">
+            <div>
+                <span>
+                    20/3/2024 
+                </span>
+                <span class="badge variant-filled ml-2 rounded-sm">Member Only</span>
+            </div>
+            <span class="text-4xl py-4 font-bold">
+                Equity Crowdfunding Malaysia
+            </span>
+            <p class="pt-2 mr-5">
+                The Securities Commission (SC) made equity crowdfunding legal in Malaysia in 2015 but only through approved operators. pitchIN is a Registered Market Operator (RMO) approved by the Securities Commission to offer equity crowdfunding. The laws governing equity crowdfunding in Malaysia is the Guidelines on Recognized Markets (‘ECF Guidelines’) issued by the SC.
+            </p>
+            <button type="button" class="btn variant-filled w-48 mt-12" on:click={() => {goto("/articles/crowdfunding")}}>Read More</button>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg basis-1/5" src="/src/lib/equityCrowdfunding.png" alt="">
+        </div>
+    </div>
+    <!-- END ITEM 1 -->
+    <!-- ITEM 2 -->
+    <span class="horizontalLine my-12"/>
+    <div class="flex flex-row px-32">
+        <div class="flex flex-col basis-4/5 justify-center">
+            <div>
+                <span>
+                    20/3/2024 
+                </span>
+                <span class="badge variant-filled ml-2 rounded-sm">Member Only</span>
+            </div>
+            <span class="text-4xl py-4 font-bold">
+                Equity Crowdfunding Malaysia
+            </span>
+            <p class="pt-2 mr-5">
+                The Securities Commission (SC) made equity crowdfunding legal in Malaysia in 2015 but only through approved operators. pitchIN is a Registered Market Operator (RMO) approved by the Securities Commission to offer equity crowdfunding. The laws governing equity crowdfunding in Malaysia is the Guidelines on Recognized Markets (‘ECF Guidelines’) issued by the SC.
+            </p>
+            <button type="button" class="btn variant-filled w-48 mt-12" on:click={() => {goto("/articles/crowdfunding")}}>Read More</button>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg basis-1/5" src="/src/lib/equityCrowdfunding.png" alt="">
+        </div>
+    </div>
+    <!-- END ITEM 2 -->
+
+</div>
+
+<style>
+    .horizontalLine {  
+        height:2px;
+        width:95%;
+        border: none; 
+        border-bottom: 1px solid rgba(220, 220, 220, 0.459);  
+    }
+</style>
