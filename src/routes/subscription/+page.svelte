06259f18fb0cd503291ed9487bde435ecfaaf7e7
Iyad Najmi <1201203090@student.mmu.edu.my>
2024-03-23 01:39:46 +0800

    Subscription page added

---
+++ src/routes/subscription/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+</script>
+
+<div class="body h-screen grid grid-cols-2 grid-rows-4 justify-items-center text-center font-sans">
+    <h1 class="col-start-1 col-span-2 m-auto text-7xl "> Subscription Plans</h1>
+<div class="flex space-x-24 justify-center w-3/4 h-3/4 col-start-1 col-span-3 m-auto my-0">
+
+    <div class="card card-hover my-auto w-1/4 p-10 rounded-sm">
+        <header class="card-header text-3xl">Monthly</header>
+        <section class="p-4 text-8xl">RM 30</section>
+        <footer class="card-footer text-lg mb-6">per month</footer>
+        <footer class="card-footer text-xl">You Get:</footer>
+        <footer class="card-footer text-2xl">Access to member exclusive content</footer>
+        <button type="button" class="btn variant-filled m-10 mb-5 rounded-sm w-4/5 h-20 text-xl">Choose It</button>
+    </div>
+
+    <div class="card card-hover my-auto w-1/4 p-10 rounded-sm ">
+        <header class="card-header text-3xl">Yearly</header>
+        <section class="p-4 text-8xl">RM 250</section>
+        <footer class="card-footer text-lg mb-6">per year</footer>
+        <footer class="card-footer text-xl">You Get:</footer>
+        <footer class="card-footer text-2xl">Access to member exclusive content</footer>
+        <footer class="card-footer text-2xl">Save more than 30% off from the Monthly Subscription plan!</footer>
+        <button type="button" class="btn variant-filled m-10 mb-5 w-4/5 rounded-sm h-20 text-xl">Choose It</button>
+    </div>
+    
+</div>
+</div>
