151ed5a581cccda97574bc3fed76b092332b07dd
Iyad Najmi <1201203090@student.mmu.edu.my>
2024-03-24 16:09:28 +0800

    .

--- src/routes/subscription/+page.svelte
+++ src/routes/subscription/+page.svelte
@@ -6,7 +6,7 @@
     <h1 class="col-start-1 col-span-2 m-auto text-7xl "> Subscription Plans</h1>
 <div class="flex space-x-24 justify-center w-3/4 h-3/4 col-start-1 col-span-3 m-auto my-0">
 
-    <div class="card card-hover my-auto w-1/4 p-10 rounded-sm">
+    <div class="card card-hover my-auto w-1/3 p-10 rounded-sm">
         <header class="card-header text-3xl">Monthly</header>
         <section class="p-4 text-8xl">RM 30</section>
         <footer class="card-footer text-lg mb-6">per month</footer>
@@ -15,7 +15,7 @@
         <button type="button" class="btn variant-filled m-10 mb-5 rounded-sm w-4/5 h-20 text-xl">Choose It</button>
     </div>
 
-    <div class="card card-hover my-auto w-1/4 p-10 rounded-sm ">
+    <div class="card card-hover my-auto w-1/3 p-10 rounded-sm ">
         <header class="card-header text-3xl">Yearly</header>
         <section class="p-4 text-8xl">RM 250</section>
         <footer class="card-footer text-lg mb-6">per year</footer>
