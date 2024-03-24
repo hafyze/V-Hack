16cd400c443f241de9861273016346c92ce68adf
hafyze <hafizmatqwer@gmail.com>
2024-03-24 12:55:32 +0800

    link stories to homepage

--- src/components/HomeSection4.svelte
+++ src/components/HomeSection4.svelte
@@ -10,7 +10,7 @@
         <div>
             <h4 class="font-bold text-xl">Success Stories</h4>
             <p class="my-4">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.</p>
-            <ButtonLike>Learn More</ButtonLike>
+            <ButtonLike hrefLink="/stories">Learn More</ButtonLike>
         </div>
         <div>
             <h4 class="font-bold text-xl">Book Recommendations</h4>
